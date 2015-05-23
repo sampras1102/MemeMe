d60f3b7502f3d00d2b58d35adc0f52d5b711261e
sampras1102 <chris.supranowitz@gmail.com>
2015-05-22 21:08:32 -0400

    Auto constraints working for instruction label

--- InstructionLabelUtils.swift
+++ InstructionLabelUtils.swift
@@ -12,7 +12,7 @@ import UIKit
 class InstructionLabelUtils
 {
     //TODO: Implement this as a new ViewController class and make the collection and table views inherit from this instead of UIViewController.  It will be much cleaner that way and I can also ensure that it gets re-laid out upon rotation
-    class func createLabelInViewController(view: UIViewController, text: String) -> UILabel
+    class func createLabelInViewController(viewController: UIViewController, text: String) -> UILabel
     {
         var label = UILabel()
         label.font = UIFont.systemFontOfSize(12)
@@ -22,16 +22,24 @@ class InstructionLabelUtils
         label.textColor = UIColor.blackColor()
         label.layer.zPosition = 1000
         label.sizeToFit()
-        view.view.addSubview(label)
-        centerLabelInViewFrame(label)
+        label.setTranslatesAutoresizingMaskIntoConstraints(false)
+        viewController.view.addSubview(label)
+        //centerLabelInViewFrame(label)
+        
+        let xCenterConstraint = NSLayoutConstraint(item: label, attribute: .CenterX, relatedBy: .Equal, toItem: viewController.view, attribute: .CenterX, multiplier: 1, constant: 0)
+        viewController.view.addConstraint(xCenterConstraint)
+        
+        let yCenterConstraint = NSLayoutConstraint(item: label, attribute: .CenterY, relatedBy: .Equal, toItem: viewController.view, attribute: .CenterY, multiplier: 1, constant: 0)
+        viewController.view.addConstraint(yCenterConstraint)
+        
         return label
     }
     
     class func centerLabelInViewFrame(label: UILabel){
-        if let v = label.superview {
-            var frameSize = v.frame
-            label.center = CGPointMake(frameSize.width/2.0, frameSize.height/2.0)
-            label.bringSubviewToFront(v)
-        }
+//        if let v = label.superview {
+//            var frameSize = v.frame
+//            label.center = CGPointMake(frameSize.width/2.0, frameSize.height/2.0)
+//            label.bringSubviewToFront(v)
+//        }
     }
 }
